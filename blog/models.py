6e4e386980b26bbf66472065b41457c5d11e2a14
gulcinkulunk <gulcin.kulunk@gmail.com>
2018-05-01 22:38:35 +0300

    güncelleme

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date =models.DateTimeField(
@@ -16,4 +16,7 @@ class Post(models.Model):
 		self.save()
 
 	def  __str__(self):
-		return self.title
+		return self.title 
+
+
+
